da9e46c20410a230641bb9d360a89392d1b21dbc
Guy Shaviv <guy@shaviv.org>
2023-11-07 17:19:12 +0200

    Revert "remove protocl"
    This reverts commit 08ac42189d7a185eeebb29eb24544763354b5749.

--- Sources/PostgresClientORM/RowWriter.swift
+++ Sources/PostgresClientORM/RowWriter.swift
@@ -19,12 +19,14 @@ public class RowWriter {
     case partialUpdate
   }
 
-  public func container<Key>(keyedBy type: Key.Type) -> TypedRowWriterContainer<Key> where Key: CodingKey {
-    TypedRowWriterContainer<Key>(type: type, codingPath: codingPath,
-                                 appendValues: { [weak self] name, value in
-                                   self?.variableNames.append(name)
-                                   self?.values.append(value)
-                                 })
+  public func container<Key>(keyedBy type: Key.Type) -> KeyedEncodingContainer<Key> where Key: CodingKey {
+    KeyedEncodingContainer(
+      SQLKeyedEncodingContainer<Key>(type: type, codingPath: codingPath,
+                                appendValues: { [weak self] name, value in
+                                  self?.variableNames.append(name)
+                                  self?.values.append(value)
+                                })
+    )
   }
   
   public func encode<T: TableObject>(_ value: T, as queryType: RowWriter.QueryType) throws -> SQLQuery<T> {
@@ -43,86 +45,103 @@ public class RowWriter {
   }
 }
 
-public struct TypedRowWriterContainer<K: CodingKey> {
+private struct SQLKeyedEncodingContainer<K: CodingKey>: KeyedEncodingContainerProtocol {
+  typealias Key = K
   var type: K.Type
-  public var codingPath: [CodingKey]
+  var codingPath: [CodingKey]
   var appendValues: (String, String) -> Void
 
-  public mutating func encodeNil(forKey key: K) throws {
+  mutating func encodeNil(forKey key: K) throws {
     appendValues(key.stringValue, "NULL")
   }
   
-  public mutating func encode(_ value: Bool, forKey key: K) throws {
+  mutating func encode(_ value: Bool, forKey key: K) throws {
     try appendValues(key.stringValue, value.postgresValue.string())
   }
   
-  public mutating func encode(_ value: String, forKey key: K) throws {
+  mutating func encode(_ value: String, forKey key: K) throws {
     appendValues(key.stringValue, "'\(value)'")
   }
   
-  public mutating func encode(_ value: Double, forKey key: K) throws {
+  mutating func encode(_ value: Double, forKey key: K) throws {
     try appendValues(key.stringValue, value.postgresValue.string())
   }
   
-  public mutating func encode(_ value: Float, forKey key: K) throws {
+  mutating func encode(_ value: Float, forKey key: K) throws {
     try appendValues(key.stringValue, Double(value).postgresValue.string())
   }
   
-  public mutating func encode(_ value: Int, forKey key: K) throws {
+  mutating func encode(_ value: Int, forKey key: K) throws {
     try appendValues(key.stringValue, value.postgresValue.string())
   }
   
-  public mutating func encode(_ value: Int8, forKey key: K) throws {
+  mutating func encode(_ value: Int8, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: Int16, forKey key: K) throws {
+  mutating func encode(_ value: Int16, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: Int32, forKey key: K) throws {
+  mutating func encode(_ value: Int32, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: Int64, forKey key: K) throws {
+  mutating func encode(_ value: Int64, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: UInt, forKey key: K) throws {
+  mutating func encode(_ value: UInt, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: UInt8, forKey key: K) throws {
+  mutating func encode(_ value: UInt8, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: UInt16, forKey key: K) throws {
+  mutating func encode(_ value: UInt16, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: UInt32, forKey key: K) throws {
+  mutating func encode(_ value: UInt32, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
   
-  public mutating func encode(_ value: UInt64, forKey key: K) throws {
+  mutating func encode(_ value: UInt64, forKey key: K) throws {
     appendValues(key.stringValue, "\(value)")
   }
       
-  public mutating func encode(_ value: some FieldSubset, forKey key: K) throws {
-    let enc = RowWriter()
-    try value.encode(row: enc)
-    zip(enc.variableNames, enc.values).forEach {
-      appendValues("\(key.stringValue)_\($0.0)", $0.1)
+  mutating func encode(_ value: some Encodable, forKey key: K) throws {
+    if let value = value as? any FieldSubset {
+      let enc = RowWriter()
+      try value.encode(row: enc)
+      zip(enc.variableNames, enc.values).forEach {
+        appendValues("\(key.stringValue)_\($0.0)", $0.1)
+      }
+    } else {
+      let enc = JSONEncoder()
+      let data = try enc.encode(value)
+      if let str = String(data: data, encoding: .utf8)?.trimmingCharacters(in: CharacterSet(charactersIn: "\"")) {
+        appendValues(key.stringValue, "'\(str)'")
+      } else {
+        throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: codingPath + [key], debugDescription: "Invalid JSON"))
+      }
     }
   }
   
-  public mutating func encode(_ value: some Encodable, forKey key: K) throws {
-    let enc = JSONEncoder()
-    let data = try enc.encode(value)
-    if let str = String(data: data, encoding: .utf8)?.trimmingCharacters(in: CharacterSet(charactersIn: "\"")) {
-      appendValues(key.stringValue, "'\(str)'")
-    } else {
-      throw EncodingError.invalidValue(value, EncodingError.Context(codingPath: codingPath + [key], debugDescription: "Invalid JSON"))
-    }
+  mutating func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type, forKey key: K) -> KeyedEncodingContainer<NestedKey> where NestedKey: CodingKey {
+    fatalError("Unsupported")
+  }
+  
+  mutating func nestedUnkeyedContainer(forKey key: K) -> UnkeyedEncodingContainer {
+    fatalError("Unsupported")
+  }
+  
+  mutating func superEncoder() -> Encoder {
+    fatalError("Unsupported")
+  }
+  
+  mutating func superEncoder(forKey key: K) -> Encoder {
+    fatalError("Unsupported")
   }
 }
